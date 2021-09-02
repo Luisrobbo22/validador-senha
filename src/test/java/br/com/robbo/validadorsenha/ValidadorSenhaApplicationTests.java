0eb177436ec222e7668617bafac9272285ea1064
Luis Felipe Robbo <61969628+Luisrobbo22@users.noreply.github.com>
2021-09-02 19:16:22 -0300

    Initial Commit

---
+++ src/test/java/br/com/robbo/validadorsenha/ValidadorSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robbo.validadorsenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
